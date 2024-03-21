072d82d08ca0a745f9df50d90377bef7bad9238c
Huseyin Emre Bolum <huseyinemrebolum@gmail.com>
2024-03-22 00:06:33 +0100

    commit

--- deneme.py
+++ deneme.py
@@ -1,19 +1,29 @@
 import os 
+from datetime import datetime, timedelta
 from random import randint
 
-for i in range(1, 140):
 
-    for j in range(0,randint(1,10)):
-        d = str(i) + 'days ago'
-    with open('file.txt','a') as file: 
-        file.write(d)
-    os.system('git add .')
-    os.system('git commit --date="'+ d + '" -m "commit "')
+today = datetime.today()
+
+
+weekend_days = [5, 6]  
+
+for i in range(1, 70):
+    for j in range(0, randint(1, 5)):
+      
+        current_date = today - timedelta(days=i)
+        
+       
+        if current_date.weekday() not in weekend_days:
+            
+            commit_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+            
+          
+            os.system('git add .')
+            os.system('git commit --date="' + commit_date + '" -m "commit"')
+
 
 os.system('git push -u origin main')
-
-
-        
 
 
 
